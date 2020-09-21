73e46ade9ad96203bab12a71aae074be753d88eb
carl salem <71038407+Flextapes@users.noreply.github.com>
2020-09-21 12:13:26 -0700

    Exercices finient.

--- exercice.py
+++ exercice.py
@@ -1,29 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import math
 from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    nombre_in = input("entre un nombre")
+    nombre = float(nombre_in)
+    convert_to_absolute = math.fabs(nombre)
+    print (convert_to_absolute)
+    return 
 
 
 def use_prefixes() -> List[str]:
-    prefixes, suffixes = 'JKLMNOP', 'ack'
+    prefixes, suffixes = 'JKLMNOPQ', 'ack'
+    liste_de_noms = []
+    for I in prefixes:
+        print(I + suffixes)
+        liste_de_noms.append(I+suffixes)         
+    return liste_de_noms
 
-    return [""]
 
+def prime_integer_summation() -> int:   
+    i=2
+    primes=[]
+    
+    while len(primes) < 100:
+        prime = True
 
-def prime_integer_summation() -> int:
-    return 0
+        for divider in range(2, int(math.sqrt(i))+1):
+            if i % divider == 0:
+                prime = False
+        if prime:
+            primes.append(i)
+        i+=1
+    return sum(primes)
 
 
 def factorial(number: int) -> int:
-    return 0
+    our_number = float(number)
+    return math.factorial(our_number)
 
 
 def use_continue() -> None:
+    for i in range(1,11):
+        if i == 5:
+            continue
+        print(i, end=" ")
     pass
 
 
@@ -32,7 +56,7 @@ def main() -> None:
 
     print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
 
-    print(f"La somme des nombres de 0 à 100 est: {prime_integer_summation()}")
+    print(f"La somme 100 premiers nombres entiers est: {prime_integer_summation()}")
 
     number = 10
     print(f"La factiorelle du nombre {number} est: {factorial(number)}")
